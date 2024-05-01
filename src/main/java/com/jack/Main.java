3ca0cd8183c7a7ef40b11d69ede78f6b2f61792b
LAPTOP-ELBIQ849\Jack <2956410096@qq.com>
2024-05-01 21:34:00 +0800

    建立空项目

---
+++ src/main/java/com/jack/Main.java
@@ -0,0 +1,12 @@
+package com.jack;
+
+/**
+ * @Description
+ * @Author Jack
+ * @Date 2024/5/1 21:32
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
