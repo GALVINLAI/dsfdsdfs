da231a7e94ac0fe1732c94b0925dca9001e14e6b
LAPTOP-ELBIQ849\Jack <2956410096@qq.com>
2024-05-03 15:57:25 +0800

    jcl 初始化

---
+++ log-jcl/src/test/java/com/jack/jcl/LogJclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.jcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogJclApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
